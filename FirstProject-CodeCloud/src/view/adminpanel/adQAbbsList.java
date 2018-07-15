ff80631ec4c73c17c66f234024cefbc2c45dd90d
seyoung <sy3253@gmail.com>
2018-07-15 17:18:09 +0900

    no message

--- FirstProject-CodeCloud/src/view/adminpanel/adQAbbsList.java
+++ FirstProject-CodeCloud/src/view/adminpanel/adQAbbsList.java
@@ -44,7 +44,7 @@ public class adQAbbsList extends JPanel implements ActionListener, MouseListener
 		adQAmian = QA;
 
 		Singleton s = Singleton.getInstance();
-		/*list = s.qaDao.getbbsList();
+/*		list = s.qaDao.getbbsList();
 
 		rowData = new Object[list.size()][4];// 테이블의 2차원배열이 생성
 
